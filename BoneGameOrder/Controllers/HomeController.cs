f246ff0b964563df830ece6c9f7fa38d38ab94f9
Badrangui A <abadrangui@gmail.com>
2020-03-18 23:33:37 +0800

    first commit

---
+++ BoneGameOrder/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BoneGameOrder.DAL;
+using BoneGameOrder.Models;
+
+namespace BoneGameOrder.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Upload()
+        {
+            return View();
+        }
+        public ActionResult SeeAvailable()
+        {
+            BoneGameEntities boneGameDB = new BoneGameEntities();
+            ViewBag.Orders = (from a in boneGameDB.GameOrders
+                              where a.StartDate.Year == DateTime.Today.Year &&
+                              a.StartDate.Month == DateTime.Today.Month &&
+                              a.StartDate.Day == DateTime.Today.Day
+                              select a).ToList();
+            return View();
+        }
+        public ActionResult Order()
+        {
+            BoneGameEntities boneGameDB = new BoneGameEntities();
+            ViewBag.Orders = (from a in boneGameDB.GameOrders
+                              where a.StartDate.Year == DateTime.Today.Year &&
+                              a.StartDate.Month == DateTime.Today.Month &&
+                              a.StartDate.Day == DateTime.Today.Day
+                              select a).ToList();
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BoneGameOrder/Models/GameOrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BoneGameOrder.Models
+{
+    public class GameOrderModel
+    {
+        public Guid ID { get; set; }
+        public string RoomNo  {get; set;}
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+    }
+}
